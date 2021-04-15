f75fab0979c0fa12923651e5a9a414ac7a4e9696
Simone-Funtanilla <simone.funtanilla180330@my.lsgh.edu.ph>
2021-04-15 15:26:47 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bmi-calculator-1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/bmi-calculator-1)
---
+++ pages/home/home.ts
@@ -0,0 +1,22 @@
+export class Homepage {
+  height: number;
+  weight: number;
+  bmi: number;
+  bmiMessage: string;
+ 
+ calculateBMI() {
+   this.bmi = this.weight / (this.height * this.height);
+   this.bmi = parseFloat(this.bmi.toFixed(2));
+
+   if (this.bmi <18.5) {
+     this.bmiMessage = "underweight";
+   } else if (this.bmi > 18.5 && this.bmi < 25) {
+     this.bmiMessage = "Normal";
+   } else if (this.bmi > 25 && this.bmi < 30) {
+     this.bmiMessage = "overweight";
+   } else { 
+     this.bmiMessage = "obese"; 
+   }
+ }
+}
+
